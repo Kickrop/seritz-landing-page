60c58975095286c0691e6db4fdf20cd7bbba26ea
den <kickrop@gmail.com>
2018-08-06 23:00:14 +0300

    Some changes
    tabs, portfolio, contacts, js-tabs

--- js/script.js
+++ js/script.js
@@ -7,14 +7,16 @@ function openTab(tabName, elmnt) {
     }
 
     // Remove the background color of all tablinks/buttons
-    // tablinks = document.getElementsByClassName("tablink");
-    // for (i = 0; i < tablinks.length; i++) {
-    //     tablinks[i].style.backgroundColor = "";
-    // }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.color = "";
+    }
 
     // Show the specific tab content
     document.getElementById(tabName).style.display = "block";
-
+    elmnt.style.backgroundColor = "#333";
+    elmnt.style.color = "#f1ad1d";
     // Add the specific color to the button used to open the tab content
     // elmnt.style.backgroundColor = color;
 }
